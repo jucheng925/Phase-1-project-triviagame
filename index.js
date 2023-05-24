563402f7f8d7a4c4f03434c0ce048b7e0bf50311
Julie C <jucheng925@gmail.com>
2023-05-24 12:51:45 -0400

    add more context in css

---
+++ index.js
@@ -0,0 +1,9 @@
+//addevent listener -domcontented loaded
+//addevent listener -submit button function - add as a heading? "hello so so"
+//timer - need a start button - will start timer and give you the first quesiton
+//question randomialzation - will random the id number - fetch it from server and then display on site
+//add event listener - once user click on an answer - whether its wrong or correct, next question will appear
+    //at the same time, it will be marked as correct or wrong, will indicate in the scoring section
+
+//afte 60 secs, will add user name and score on the scoring list on the bottom - using post so it will go back to the server
+
