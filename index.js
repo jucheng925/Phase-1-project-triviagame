df8872d527f4bf372a05be88b71772e0a1a0e028
Julie C <jucheng925@gmail.com>
2023-05-25 11:50:11 -0400

    building form to include player name

--- index.js
+++ index.js
@@ -5,5 +5,28 @@
 //add event listener - once user click on an answer - whether its wrong or correct, next question will appear
     //at the same time, it will be marked as correct or wrong, will indicate in the scoring section
 
-//afte 60 secs, will add user name and score on the scoring list on the bottom - using post so it will go back to the server
+//after 60 secs, will add user name and score on the scoring list on the bottom - using post so it will go back to the server
 
+
+//Check to see if this is needed?
+document.addEventListener("DOMContentLoaded", ()=> {
+    document.querySelector("form").addEventListener("submit", (e) => {
+        e.preventDefault()
+        handleName(e.target.player_name.value)
+    })
+})
+
+function handleName(name) {
+    console.log(name)
+    document.querySelector("#name").textContent = name
+    document.querySelector("#greetings").style.display = "block"
+    document.querySelector("form").style.display = "none"
+    handleStart(document.querySelector("button"))
+}
+
+
+function handleStart(button) {
+    button.addEventListener("click", (e)=> {
+        console.log(e)
+    })
+}
